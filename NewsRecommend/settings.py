8e9a40ec5c8909a0f06013661fb4276bd7ae5be3
yang <codedevyang@qq.com>
2023-01-02 22:45:05 +0800

    配置MYSQL

--- NewsRecommend/settings.py
+++ NewsRecommend/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'NewsRecommend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'news',
+        'USER': 'root',
+        'PASSWORD': '123456',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
--- web/models.py
+++ web/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):#继承模型类
+    uname=models.CharField(max_length=10)#设置列名
+    upwd=models.CharField(max_length=10)
+    class Meta:#设置要存储的数据库名称,默认按照父类的名称创建
+        db_table='users'
